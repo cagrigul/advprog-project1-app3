c3bfb0e21de56ebcfce7f9f8c0ebfdab169ab0fb
cagrigul <123596574+cagrigul@users.noreply.github.com>
2024-02-07 23:06:52 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,45 @@
+require([
+  "esri/WebScene",
+  "esri/views/SceneView",
+  "esri/widgets/Home",
+  "esri/widgets/Legend",
+  "esri/widgets/LayerList",
+  "esri/widgets/Search",
+  "esri/layers/FeatureLayer"
+], function(WebScene, SceneView, Home, Legend, LayerList, Search, FeatureLayer) {
+  
+  var scene = new WebScene({
+    portalItem: {
+      id: "b53bf05e5362403abb48837050967c00"
+    }
+  });
+  
+ var view = new SceneView({
+  container: "viewDiv",
+  map: scene,
+  environment: {
+    lighting: {
+      date: new Date('2024-02-07T18:00:00Z'),
+      directShadowsEnabled: false, 
+      cameraTrackingEnabled: false
+    }
+  }
+});
 
+  var homeBtn = new Home({
+    view: view
+  });
+  view.ui.add(homeBtn, "top-left");
+
+  var legend = new Legend({
+    view: view
+  });
+  view.ui.add(legend, "bottom-right");
+
+  // 
+
+  var layerList = new LayerList({
+    view: view
+  });
+  view.ui.add(layerList, "top-right");
+});
